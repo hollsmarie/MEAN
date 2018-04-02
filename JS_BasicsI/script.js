acbd5a4c13c52a85cc4aebec2cb9162409f33f76
Holly Valenty <holly914@gmail.com>
2018-04-02 12:22:07 -0500

    first commit

---
+++ JS_BasicsI/script.js
@@ -0,0 +1,78 @@
+//Basic I
+
+//Create a variable x as an empty array []. Log this array to your console.
+// var x = [];
+// console.log(x);
+
+// Use .push() to add three strings to your array: 'coding', 'dojo', 'rocks'.
+// let x = [];
+// x.push('coding')
+// x.push('dojo') 
+// x.push('rocks');
+// console.log(x);
+
+// Use .pop() to remove the final element of your array.
+// let x = ['coding', 'dojo', 'rocks'];
+// x.pop();
+// console.log(x);
+
+// Log the final value of x, what is it?
+// let x = ['coding', 'dojo', 'rocks'];
+// console.log(x[x.length-1]);
+
+
+// Basic II
+// Create a const called y, and store an empty array there. Log this to your console.
+// const y = [];
+// console.log(y);
+
+// Use .push() to add the number 88 to array y.
+// const y = [];
+// y.push(88);
+// console.log(y);
+
+// // What happened?
+// It allowed me to add it. You can add and take away,  but only declare once. Not immutable.
+
+
+// Basic III
+// Create a variable z that contains the array [9, 10, 6, 5, -1, 20, 13, 2].
+// var z = [9, 10, 6, 5, -1, 20, 13, 2];
+
+// Print every element to the console.
+// console.log(z);
+
+// Now print every element except the final number.
+// z.pop();
+// console.log(z);
+
+
+// Basic IV
+// Create a variable names and store the array ['Kadie', 'Joe', 'Fritz', 'Pierre', 'Alphonso']
+// var names = ['Kadie', 'Joe', 'Fritz', 'Pierre', 'Alphonso'];
+
+// Print the length of all names to the console.
+// console.log(names.length);
+
+// Now modify your code so only names with a length of 5 print.
+// var names = ['Kadie', 'Joe', 'Fritz', 'Pierre', 'Alphonso'];
+// for (var name of names)
+// {
+//     if (name.length == 5)
+//     console.log(name);
+// }
+
+
+// Basic V
+// Create a function yell that takes one parameter called string.
+// function yell(str)
+// {
+//     var str = "this is a string.";
+//     var res = str.toUpperCase();
+//     console.log(res);
+// }
+// yell();
+
+// Make yell return that string in all uppercase.
+
+// Where could you look to find out more about uppercasing strings in JavaScript?
